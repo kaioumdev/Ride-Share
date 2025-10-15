5e8c9f5672f07aebc4e82065756aeec517ba937a
kaioumdev <afrankaiyum34@gmail.com>
2025-10-15 22:13:56 +0600

    add .env file to the backend

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hellow World");
